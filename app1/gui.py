f9819b3ef4ebeee91c40ebe5af52a6b38eb2ac16
misha <misha07.sharma@gmail.com>
2024-09-26 23:09:09 +0530

    Implemented the Add Button

--- app1/gui.py
+++ app1/gui.py
@@ -1,11 +1,33 @@
-# from modules import functions
-import FreeSimpleGUI as sg
 
+from modules import functions
+
+
+import FreeSimpleGUI as sg  #This is third party library/moduless
 
 label = sg.Text("Type in a To-do")
-input_box =  sg.InputText(tooltip="Enter the to-do")
+input_box =  sg.InputText(tooltip="Enter the to-do",key= "todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To- Do App ", layout= [[label],[input_box,add_button]])
-window.read()
+window = sg.Window("My To- Do App ", layout= [[label],[input_box],[add_button]], font=("Helvetica",20))
+
+while True :
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event :
+        case "Add":
+            todos  = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WINDOW_CLOSED:  #this is for closing X button will not throw error and break the loop once user click on cross in gui
+            break
+
+
+
+
+
+
+
+
 window.close()
